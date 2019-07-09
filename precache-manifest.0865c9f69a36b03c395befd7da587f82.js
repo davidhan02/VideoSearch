5b14b306a86ea4a228beeb3ef3ffae5352f44fc2
David Han <davidhan.5.25@gmail.com>
2019-07-08 21:35:09 -0700

    Updates

---
+++ precache-manifest.0865c9f69a36b03c395befd7da587f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25dba3b2a298ad2500f0",
+    "url": "/VideoSearch/static/js/runtime~main.c6d0195f.js"
+  },
+  {
+    "revision": "a892d41a801201d8c871",
+    "url": "/VideoSearch/static/js/main.fdeba115.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/VideoSearch/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "a892d41a801201d8c871",
+    "url": "/VideoSearch/static/css/main.7de20d4a.chunk.css"
+  },
+  {
+    "revision": "7f0c197d73cfe536a494e3d04be13cb4",
+    "url": "/VideoSearch/index.html"
+  }
+];
